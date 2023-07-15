ee3dd003c80e01217a7438b623229abccfa01b51
sasha_kalinin <kalininsasa38@gmail.com>
2023-07-15 15:38:30 +0300

    parsing done

--- parcing_site_culture/main.py
+++ parcing_site_culture/main.py
@@ -3,31 +3,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# url = "https://www.culture.ru/literature/poems/tag-o-lyubvi"
 headers = {
     "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
-# req = requests.get(url=url, headers=headers)
-# src = req.text
-#
-# with open("index.html", "w", encoding="utf-8") as file:
-#     file.write(src)
 
-# with open("index.html", encoding="utf-8") as file:
-#     src = file.read()
+# for page_number in range(1, 109):
+#     url = "https://www.culture.ru/literature/poems/tag-o-lyubvi?page=" + str(page_number)
+#     req = requests.get(url=url, headers=headers)
+#     src = req.text
+#     with open(f"html/{page_number}.html", "w", encoding="utf-8") as file:
+#         file.write(src)
+#     print(f"{page_number} страница скопирована")
 
 result = dict()
-pages_list = list()
-for page_number in range(1, 109):
-    url = "https://www.culture.ru/literature/poems/tag-o-lyubvi?page=" + str(page_number)
-    pages_list.append(url)
-
-for page_url in pages_list:
-    req = requests.get(page_url, headers)
-    src = req.text
-    print(src)
+for index in range(1, 109):
+    with open(f"html/{index}.html", "r", encoding="utf-8") as file:
+        src = file.read()
     soup = BeautifulSoup(src, "lxml")
     find_all_hrefs = soup.find(class_="A5Si4").find_all(class_="bMNap")
 
@@ -36,7 +29,6 @@ for page_url in pages_list:
         item_text_string = " ".join(item_text.split())
         item_url = "https://www.culture.ru" + item.find(class_="_1ERrb").find(class_="_9OVEn").get("href")
         result[item_text_string] = item_url
-        print(result)
 
 with open("result.json", "w") as file:
     json.dump(result, file, indent=4, ensure_ascii=False)
